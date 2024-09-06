a6da10ba906391466b26535107e69f0ac32e2a47
Ivan <i.popovych.work@gmail.com>
2024-09-06 19:15:03 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: 'https://justkooker.github.io/Property_House_test/',
+  base: '/Property_House_test/',
 });
